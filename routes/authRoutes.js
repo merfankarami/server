4a2512421e652817e791e15474b1ec81a8477b95
Erfan Karami <mohammaderfankarami456@gmail.com>
2020-06-26 14:22:33 +0430

    commited

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,6 +10,11 @@ module.exports = (app) => {
 
   app.get("/auth/google/callback", passport.authenticate("google"));
 
+  app.get("/api/logout", (req, res) => {
+    req.logout();
+    res.send(req.user);
+  })
+
   app.get("/api/current_user", (req, res) => {
     console.log("asdasda");
     res.send(req.user);
